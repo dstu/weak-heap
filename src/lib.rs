92b08ce3c0b17d0f10838c96aac9759903f126c9
Stu Black <trurl@freeshell.org>
2018-01-11 23:35:16 -0500

    Switch to Vec<bool> from BitVec to get speedup almost to parity with BinaryHeap.

--- src/lib.rs
+++ src/lib.rs
@@ -2,126 +2,32 @@
 #![feature(inclusive_range_syntax)]
 #![feature(test)]
 
-extern crate bit_vec;
 #[cfg(test)] extern crate rand;
 #[cfg(test)] extern crate test;
 
 #[cfg(test)] mod bench;
 
-use bit_vec::BitVec;
 use std::cmp::Ord;
 use std::fmt;
-use std::mem;
 use std::ptr;
-
-const USIZE_BITS: u32 = (mem::size_of::<usize>() * 8) as u32;
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-enum EdgeValence {
-  Standard,
-  Flipped,
-}
-
-impl From<bool> for EdgeValence {
-  fn from(b: bool) -> EdgeValence {
-    if b {
-      EdgeValence::Flipped
-    } else {
-      EdgeValence::Standard
-    }
-  }
-}
-
-impl Into<bool> for EdgeValence {
-  fn into(self) -> bool {
-    match self {
-      EdgeValence::Flipped => true,
-      EdgeValence::Standard => false,
-    }
-  }
-}
-
-impl Into<usize> for EdgeValence {
-  fn into(self) -> usize {
-    match self {
-      EdgeValence::Flipped => 1,
-      EdgeValence::Standard => 0,
-    }
-  }
-}
-
-fn lg_floor_1p(n: usize) -> usize {
-  let n = n + 1;
-  let trailing_zeros = n.trailing_zeros();
-  let leading_zeros = n.leading_zeros();
-  if trailing_zeros + leading_zeros + 1 == USIZE_BITS {
-    // Perfect power of 2.
-    trailing_zeros as usize
-  } else {
-    (USIZE_BITS - leading_zeros - 1) as usize
-  }
-}
-
-struct ValenceVec(BitVec);
-
-impl ValenceVec {
-  fn new() -> Self {
-    ValenceVec(BitVec::new())
-  }
-
-  fn with_capacity(cap: usize) -> Self {
-    ValenceVec(BitVec::with_capacity(cap))
-  }
-
-  fn get(&self, index: usize) -> Option<EdgeValence> {
-    self.0.get(index).map(|b| b.into())
-  }
-
-  fn set(&mut self, index: usize, value: EdgeValence) {
-    self.0.set(index, value.into());
-  }
-
-  fn flip(&mut self, index: usize) {
-    let value = self.0.get(index).unwrap();
-    self.0.set(index, !value);
-  }
-
-  fn push(&mut self, value: EdgeValence) {
-    self.0.grow(1, value.into());
-  }
-}
-
-impl fmt::Debug for ValenceVec {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "ValenceVec[")?;
-    let mut items = (0..self.0.len()).map(|n| self.0.get(n).unwrap());
-    if let Some(b) = items.next() {
-      write!(f, "{:?}", EdgeValence::from(b))?;
-      while let Some(b) = items.next() {
-        write!(f, ", {:?}", EdgeValence::from(b))?;
-      }
-    }
-    write!(f, "]")
-  }
-}
 
 #[derive(Debug)]
 pub struct WeakHeap<T: fmt::Debug + Ord> {
-  valences: ValenceVec,
+  valences: Vec<bool>,
   data: Vec<T>,
 }
 
 impl<T: fmt::Debug + Ord> WeakHeap<T> {
   pub fn new() -> Self {
     WeakHeap {
-      valences: ValenceVec::new(),
+      valences: Vec::new(),
       data: Vec::new(),
     }
   }
 
   pub fn with_capacity(cap: usize) -> Self {
     WeakHeap {
-      valences: ValenceVec::with_capacity(cap),
+      valences: Vec::with_capacity(cap),
       data: Vec::with_capacity(cap),
     }
   }
@@ -141,10 +47,8 @@ impl<T: fmt::Debug + Ord> WeakHeap<T> {
   pub fn push(&mut self, value: T) {
     let offset = self.len();
     self.data.push(value);
-    self.valences.push(EdgeValence::Standard);
-    if offset % 2 == 0 {
-      self.valences.set(offset / 2, EdgeValence::Standard);
-    }
+    self.valences.push(false);
+    self.valences[offset / 2] = self.valences[offset / 2] || offset % 2 == 0;
     self.sift_up(offset);
   }
 
@@ -159,17 +63,15 @@ impl<T: fmt::Debug + Ord> WeakHeap<T> {
   }
 
   fn child_offset(&self, offset: usize) -> usize {
-    let valence = self.valences.get(offset).expect("no valence available for child offset");
     offset.checked_mul(2)
-      .and_then(|n| n.checked_add(Into::<usize>::into(valence)))
+      .and_then(|n| n.checked_add(self.valences[offset] as usize))
       .expect("child offset computation overflow")
   }
 
   fn sibling_offset(&self, offset: usize) -> usize {
-    let valence = self.valences.get(offset).expect("no valence available for sibling offset");
     offset.checked_mul(2)
-      .and_then(|n| n.checked_add(1 - Into::<usize>::into(valence)))
-      .expect("sibling offset computation overflow")
+      .and_then(|n| n.checked_add(1 - self.valences[offset] as usize))
+      .expect("sibling computation overflow")
   }
 
   fn distinguished_ancestor_offset(&self, mut offset: usize) -> usize {
@@ -177,11 +79,11 @@ impl<T: fmt::Debug + Ord> WeakHeap<T> {
       panic!("root has no ancestor");
     } else {
       let mut parent_offset = offset / 2;
-      let mut parent_valence = self.valences.get(parent_offset).expect("no parent valence available");
-      while (offset & 1) == Into::<usize>::into(parent_valence) {
+      let mut parent_valence = self.valences[parent_offset];
+      while (offset & 1) == (parent_valence as usize) {
         offset = parent_offset;
         parent_offset /= 2;
-        parent_valence = self.valences.get(parent_offset).expect("no parent valence available");
+        parent_valence = self.valences[parent_offset];
       }
       parent_offset
     }
@@ -197,7 +99,7 @@ impl<T: fmt::Debug + Ord> WeakHeap<T> {
           break;
         }
         ptr::copy_nonoverlapping(&self.data[ancestor_offset], &mut self.data[offset], 1);
-        self.valences.flip(offset);
+        self.valences[offset] = !self.valences[offset];
         offset = ancestor_offset;
       }
       ptr::write(&mut self.data[offset], element);
@@ -217,7 +119,7 @@ impl<T: fmt::Debug + Ord> WeakHeap<T> {
     while child_offset != offset {
       if self.data[offset] < self.data[child_offset] {
         self.data.swap(offset, child_offset);
-        self.valences.flip(child_offset);
+        self.valences[child_offset] = !self.valences[child_offset];
       }
       child_offset /= 2;
     }
@@ -233,23 +135,6 @@ mod tests {
     let seed: &[_] = &[1, 2, 3, 4];
     let mut rng: StdRng = SeedableRng::from_seed(seed);
     (0..size).map(|_| rng.gen::<i32>()).map(|x| x.into()).collect()
-  }
-
-  #[test]
-  fn lg_floor_1p() {
-    assert_eq!(0, super::lg_floor_1p(0));
-    assert_eq!(1, super::lg_floor_1p(1));
-    assert_eq!(1, super::lg_floor_1p(2));
-    assert_eq!(2, super::lg_floor_1p(3));
-    assert_eq!(2, super::lg_floor_1p(4));
-    assert_eq!(2, super::lg_floor_1p(5));
-    assert_eq!(2, super::lg_floor_1p(6));
-    assert_eq!(3, super::lg_floor_1p(7));
-    assert_eq!(3, super::lg_floor_1p(8));
-    assert_eq!(3, super::lg_floor_1p(9));
-    assert_eq!(3, super::lg_floor_1p(10));
-    assert_eq!(5, super::lg_floor_1p(60));
-    assert_eq!(6, super::lg_floor_1p(63));
   }
 
   #[test]
